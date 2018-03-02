351711aeddbc581c810a85d97263d106cac7b786
Mickael <mickael.guenot@infotel.com>
2018-03-02 16:51:33 +0100

    main

--- BackGestionBiblio/src/main/java/com/formation/gestionbiblio/main/Principal.java
+++ BackGestionBiblio/src/main/java/com/formation/gestionbiblio/main/Principal.java
@@ -1,10 +1,26 @@
 package com.formation.gestionbiblio.main;
 
+import com.infotel.gestionbiblio.entity.*;
+
 public class Principal 
 {
 
 	public static void main(String[] args) 
 	{
+		Author author = new Author();
+		Book book = new Book();
+		BookBasket bookBasket = new BookBasket();
+		BookCopy bookCopy = new BookCopy();
+		Bookshelf bookshelf = new Bookshelf();
+		Borrow borrow = new Borrow();
+		BorrowPK borrowPK = new BorrowPK();
+		Catalog catalog = new Catalog();
+		Category category = new Category();
+		Editor editor = new Editor();
+		Library library = new Library();
+		Member member = new Member();
+		Registration registration = new Registration();
+		
 		
 
 	}
