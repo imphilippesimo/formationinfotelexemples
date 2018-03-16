f0492c9a5797fe428f493788975f4a542b18c202
MickaelGUENOT <mickael.guenot@infotel.com>
2018-03-16 17:22:33 +0100

    [MICK] mise en place 1er Hibernate initialize

--- ProjetGestionBiblio/BackGestionBiblio/src/main/java/com/infotel/gestionbiblio/dao/impl/MemberDaoImpl.java
+++ ProjetGestionBiblio/BackGestionBiblio/src/main/java/com/infotel/gestionbiblio/dao/impl/MemberDaoImpl.java
@@ -2,6 +2,7 @@ package com.infotel.gestionbiblio.dao.impl;
 
 import java.util.List;
 
+import org.hibernate.Hibernate;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,45 +18,55 @@ public class MemberDaoImpl extends CommonDaoImpl<Member> implements MemberDao {
 
 	Member member;
 	List<Member> memberList;
-	
+
 	@Autowired
 	private SessionFactory sessionFactory;
 
-
-	
 	@Override
-	public Member getObjectByName(String nom) 
-	{
-		Query query= sessionFactory.getCurrentSession().
-		        createQuery("from Member where memberName=:name");
+	public Member getObjectByName(String nom) {
+		Query query = sessionFactory.getCurrentSession().createQuery("from Member where memberName=:name");
 		query.setParameter("name", nom);
 		member = (Member) query.uniqueResult();
-		
+
 		return member;
 	}
-	
+
 	@Override
-	public List<Member> getList()
-	{
+	public List<Member> getList() {
 		memberList = super.getList();
+		for (Member member : memberList) {
+			Hibernate.initialize(member.getRegistrations());
+			Hibernate.initialize(member.getBorrow());
+			Hibernate.initialize(member.getBookBasket());
+		}
 		return memberList;
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public Member getMemberByLogin(String memberEmail, String memberPassword) 
-	{
+	public Member getMemberByLogin(String memberEmail, String memberPassword) {
 		// TODO Auto-generated method stub
-		return (Member)sessionFactory.getCurrentSession().createQuery("FROM Member WHERE memberEmail=:email AND memberPassword=:password").setParameter("email", memberEmail).setParameter("password", memberPassword).getResultList().stream().findFirst().orElse(null);
-	}
-	
-	/*@SuppressWarnings("unchecked")
-	@Override
-	public boolean isMemberByLoginExist(String memberEmail, String memberPassword) 
-	{
+		Member member = (Member) sessionFactory.getCurrentSession()
+				.createQuery("FROM Member WHERE memberEmail=:email AND memberPassword=:password")
+				.setParameter("email", memberEmail).setParameter("password", memberPassword).getResultList().stream()
+				.findFirst().orElse(null);
+		Hibernate.initialize(member.getRegistrations());
+		Hibernate.initialize(member.getBorrow());
+		Hibernate.initialize(member.getBookBasket());
 
-		return (Member)sessionFactory.getCurrentSession().createQuery("FROM Member WHERE memberEmail=:email AND memberPassword=:password").setParameter("email", memberEmail).setParameter("password", memberPassword).getResultList().stream().findFirst().orElse(null);
-	}*/
+		return member;
+	}
+
+	/*
+	 * @SuppressWarnings("unchecked")
+	 * 
+	 * @Override public boolean isMemberByLoginExist(String memberEmail, String
+	 * memberPassword) {
+	 * 
+	 * return (Member)sessionFactory.getCurrentSession().
+	 * createQuery("FROM Member WHERE memberEmail=:email AND memberPassword=:password"
+	 * ).setParameter("email", memberEmail).setParameter("password",
+	 * memberPassword).getResultList().stream().findFirst().orElse(null); }
+	 */
 
 }
- 
